c391575eb6ab35c4b9d772417be31d7e55c03097
GruttePot <d.pot431@gmail.com>
2026-03-18 11:50:50 +0100

    Interface verder uitgewerkt voor de GameService

--- Set-Backend/Services/IGameService.cs
+++ Set-Backend/Services/IGameService.cs
@@ -9,4 +9,16 @@ public interface IGameService
     Task<Game> CreateGameAsync(Game game);
     Task<Game> UpdateGameAsync(Game game);
     Task DeleteGameAsync(Game game);
+    
+    Task<Deck> ShuffleDeckAsync(Deck deck);
+    Task<Card> DealCardAsync(Deck deck);
+    Task<bool> ValidateSetAsync(List<Card> cards);
+    Task<List<Card>> FindAvailableSetsAsync(Deck deck);
+    Task<List<Card>> GetHintAsync(Deck deck);
+    Task<Card> DrawCardIfNotSetAsync(Deck deck, List<Card> cards);
+    
+    bool IsValidSet(Card card1, Card card2, Card card3);
+
+    List<Card[]> FindAllSets(List<Card> tableCards);
+
 }
