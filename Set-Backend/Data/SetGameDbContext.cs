31175309be4fff04b3db7d5419198dfa47a870ce
GruttePot <d.pot431@gmail.com>
2026-03-18 11:36:09 +0100

    DbContext klasse uitgewerkt voor Players en Games

--- Set-Backend/Data/SetGameDbContext.cs
+++ Set-Backend/Data/SetGameDbContext.cs
@@ -6,20 +6,19 @@ namespace Set_Backend.Data;
 public class SetGameDbContext : DbContext
 {
     public SetGameDbContext(DbContextOptions<SetGameDbContext> options) : base(options) { }
-
-    public DbSet<Card> Cards { get; set; }
+    
     public DbSet<Player> Players { get; set; }
     public DbSet<Game> Games { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Player>()
-            .Property(p => p.Name)
+            .Property(p => p.UserName)
             .IsRequired()
             .HasMaxLength(100);
         
         modelBuilder.Entity<Game>()
-            .Property(g => g.Created)
-            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            .Property(g => g.CreatedAt)
+            .IsRequired();
     }
 }
