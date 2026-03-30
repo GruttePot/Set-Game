46b071a14629313f771003243b81c995014f1cfd
GruttePot <d.pot431@gmail.com>
2026-03-30 15:40:35 +0200

    Header opgezet voor de frontend en functioneert voor een deel.

---
+++ Set-Frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from "@angular/core";
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
+  standalone: true
+})
+
+export class HeaderComponent {
+  isAuthenticated$: Observable<boolean>;
+  constructor(private router: Router, private authService: AuthService) {
+    this.isAuthenticated$ = this.authService.isAuthenticated();
+  }
+
+  routeTo(target: string)
+  {
+    this.router.navigate([target]);
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
