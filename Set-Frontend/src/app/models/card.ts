1b5bf9252dec18fddbe4bf50362c64c584c4f150
GruttePot <d.pot431@gmail.com>
2026-03-31 15:34:48 +0200

    Card model opgezet in de Angular

---
+++ Set-Frontend/src/app/models/card.ts
@@ -0,0 +1,12 @@
+﻿export enum CardColour { Red = 'red', Green = 'green', Purple = 'purple' }
+export enum CardShape { Diamond = 'diamond', Squiggle = 'squiggle', Oval = 'oval' }
+export enum CardFilling { Solid = 'solid', Striped = 'striped', Open = 'open' }
+export enum CardNumber { One = 'one', Two = 'two', Three = 'three' }
+
+export interface Card {
+  id: number;
+  colour: CardColour;
+  shape: CardShape;
+  filling: CardFilling;
+  number: CardNumber;
+}
--- Set-Frontend/src/environments/environment.development.ts
+++ Set-Frontend/src/environments/environment.development.ts
@@ -3,5 +3,5 @@
   apiUrl: 'http://localhost:5153/api',
   authUrl: 'http://localhost:5153/api/Player/login',
   authTokenKey: 'auth_token_key',
-  cardUrlFolder: 'assets/cards/'
+  cardUrlFolder: 'assets/images/'
 }
