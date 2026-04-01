8b02fc2c229843e944aeb851bc905ea303df24fa
GruttePot <d.pot431@gmail.com>
2026-04-01 15:19:36 +0200

    AuthInterceptor toegepast

--- Set-Frontend/src/app/app.config.ts
+++ Set-Frontend/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient} from '@angular/common/http';
-
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { routes } from './app.routes';
+import {AuthInterceptor} from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     provideRouter(routes)
   ]
 };
