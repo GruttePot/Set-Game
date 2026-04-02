5a2371c20c19bc3cb47f2edf045fdb2aa62f3f26
GruttePot <d.pot431@gmail.com>
2026-04-02 15:36:45 +0200

    Home pagina is nu opgezet met een deel van de implementatie.

---
+++ Set-Frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,35 @@
+﻿import {Component, inject, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {DatePipe } from '@angular/common';
+import {PlayerService } from '../../services/player.service';
+import {Game } from '../../models/game';
+
+@Component({
+  selector: 'home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss'],
+  imports: [DatePipe],
+  standalone: true
+})
+export class HomeComponent implements OnInit {
+  playerService: PlayerService = inject(PlayerService);
+  router: Router = inject(Router);
+  games: Game[] | undefined;
+
+  async ngOnInit() {
+    await this.getGames();
+  }
+
+  routeTo(target: string): void {
+    this.router.navigate([target]);
+  }
+
+  async getGames() {
+    this.games = await this.playerService.getGames();
+  }
+
+  gotoGame(game: number) {
+    this.router.navigate(['/game-board', game]);
+  }
+
+}
